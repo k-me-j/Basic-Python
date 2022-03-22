f6f67000df1d9f8835bd2bfe7260c6d965052656
Kamie Jung <K.me4dev@gmail.com>
2022-03-22 13:51:25 +0900

    pandas.DataFrame으로 학생부 만들기 ft.강

--- f220321_hw.py
+++ f220321_hw.py
@@ -10,12 +10,16 @@ def df_ai_engr(nameList):
 
     for i in range(1, len_student):
         ai_engr.loc[i, "Name"] = nameList[i]
-        ai_engr.loc[i, "Id"] = f"2022_{i}"
-        ai_engr.loc[i, "국어"] = score()
-        ai_engr.loc[i, "수학"] = score()
-        ai_engr.loc[i, "영어"] = score()
+        ai_engr.loc[i, "Id"] = f"2022_{2}"
+        ai_engr.loc[i, "Korean"] = score()
+        ai_engr.loc[i, "Math"] = score()
+        ai_engr.loc[i, "English"] = score()
+        # ai_engr["국어"] = score()
+
+    ai_engr = ai_engr[['Korean', 'Math', 'English']].astype(int)
 
     return ai_engr
+
 
 nameList = ['신주석', '김근형', '김동혁', '남현진', '노영하',
             '박수연', '심우석', '안원영', '오수은', '이근혁',
@@ -23,12 +27,13 @@ nameList = ['신주석', '김근형', '김동혁', '남현진', '노영하',
             '정경임', '차민욱', '최민석', '최비결', '최윤정',
             '최지호', '표주혁', '허진행']
 
+
 if __name__ == "__main__":
-    print(df_ai_engr(nameList))
-    print(df_ai_engr(nameList).info())
+    df = df_ai_engr(nameList)
+    print(df)
+    print(df.info())
 
     # print(type(score()))
 
-    df_ai_engr(nameList).to_csv("D:/Kamie/mon_ML/f220321.csv")
-
+    df.to_csv("D:/Kamie/mon_ML/f220321.csv")
 
